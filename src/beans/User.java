16e209ef8dc9ae00576c9658acea52516a845c33
kevinmck91 <kevmckeon@gmail.com>
2016-11-12 13:25:50 +0000

    Create the user bean

--- src/beans/User.java
+++ src/beans/User.java
@@ -2,4 +2,37 @@ package beans;
 
 public class User {
 
+	private String email ; 
+	private String password = "";
+	private String message = "";
+	
+	////////Constructors /////////
+	public User(){
+		
+	}
+	
+	public User(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+	
+	//////////Getters and setters for username + email//////////////
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+////////////Validation ///////////////
+	public String getMessage() {
+		return message;
+	}
 }
